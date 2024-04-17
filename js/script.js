138bf44e0890872508a51a47c253ad97dbc26bde
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-17 21:49:08 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Created by: Nathan De Silva
+// Created on: April 2024
+'use strict'
+/**
+ * This function converst fahrenheit to celsius
+ */
+function myButtonClicked() {
+  // input
+  const fahrenheit = parseFloat(document.getElementById('temperature').value)
+
+  // process
+  const celsius = (fahrenheit - 32) * 5 / 9
+
+  // output
+  document.getElementById('celsius').innerHTML = 'The temperature is: ' + celsius.toFixed(2) + ' °C'
+}
